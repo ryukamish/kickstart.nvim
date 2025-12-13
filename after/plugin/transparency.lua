5e3b4f5668bfdcc99627718f38cf2a56945875b3
ryukamish <ryukamish@noreply.codeberg.org>
2025-12-13 17:11:51 +0530

    feat: custom plugins

---
+++ after/plugin/transparency.lua
@@ -0,0 +1,53 @@
+vim.opt.termguicolors = true
+
+local groups = {
+  'Normal',
+  'NormalNC',
+  'NormalFloat',
+  'EndOfBuffer',
+  'LineNr',
+  'CursorLine',
+  'CursorLineNr',
+  'SignColumn',
+  'Pmenu',
+  'PmenuSel',
+  'PmenuThumb',
+  'FloatBorder',
+  'NeoTreeNormal',
+  'NeoTreeNormalNC',
+  -- "StatusLine", "StatusLineNC",
+  -- "WinSeparator",
+  -- "TabLine", "TabLineFill", "TabLineSel"
+}
+
+local function make_transparent()
+  for _, g in ipairs(groups) do
+    -- set bg = none; keep fg untouched
+    pcall(vim.api.nvim_set_hl, 0, g, { bg = 'none' })
+  end
+
+  -- also remove any EndOfBuffer tildes color
+  pcall(vim.api.nvim_set_hl, 0, 'EndOfBuffer', { fg = nil, bg = 'none' })
+
+  -- optional: set winblend for popup menus and floating windows
+  vim.o.pumblend = 0
+  vim.o.winblend = 0
+end
+
+-- run once now (VimEnter may already have passed if loaded late)
+make_transparent()
+
+-- re-apply whenever colorscheme changes
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = function()
+    make_transparent()
+  end,
+})
+
+-- optional safety: re-apply on VimEnter (for odd ordering)
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    vim.schedule(make_transparent)
+  end,
+})
---
+++ lua/custom/plugins/neowiki.lua
@@ -0,0 +1,15 @@
+return {
+  'echaya/neowiki.nvim',
+  opts = {
+    wiki_dirs = {
+      -- neowiki.nvim supports both absolute and tilde-expanded paths
+      -- { name = 'Work', path = '~/work/wiki' },
+      { name = 'Notes', path = '~/Documents/notes' },
+    },
+  },
+  keys = {
+    { '<leader>ww', "<cmd>lua require('neowiki').open_wiki()<cr>", desc = 'Open Wiki' },
+    { '<leader>wW', "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = 'Open Wiki in Floating Window' },
+    { '<leader>wT', "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = 'Open Wiki in Tab' },
+  },
+}
